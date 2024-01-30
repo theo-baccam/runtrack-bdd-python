c0dfcffea9f126b6fdce3e64df2832d4b6dd4b90
theo-baccam <theo.baccam@laplateforme.io>
2024-01-30 10:39:38 +0100

    job 5

---
+++ job5.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+laplateforme = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="CV&$i7mx$oZDrq",
+    database="laplateforme"
+)
+
+cursor = laplateforme.cursor(buffered=True)
+cursor.execute("SELECT superficie FROM etage")
+etage = cursor.fetchall()
+total = 0
+for superficie in etage:
+    total += superficie[0]
+print(f"La superficie de La Plateforme est de {total} m2")
+cursor.close()
+
+laplateforme.close()
